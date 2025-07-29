5eaae737474452ebe1b9c527d343be6b0cda12c7
matthew-ngzc <82407622+matthew-ngzc@users.noreply.github.com>
2025-07-30 01:56:26 +0800

    added health endpoint

---
+++ backend/src/main/java/com/fastnfit/app/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.fastnfit.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public String healthz() {
+        return "OK";
+    }
+}
